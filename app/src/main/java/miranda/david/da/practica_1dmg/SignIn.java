f8e4dcf50283a10940dc06289731b2fc04fc9dc2
DavidMG2 <david_smg_92@hotmail.com>
2018-10-26 20:32:26 +0100

    Primer Commit

---
+++ app/src/main/java/miranda/david/da/practica_1dmg/SignIn.java
@@ -0,0 +1,14 @@
+package miranda.david.da.practica_1dmg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
+
