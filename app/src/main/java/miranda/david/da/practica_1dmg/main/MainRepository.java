a626259eed2c1107c792273091e09835cf3f21b4
DavidMG2 <david_smg_92@hotmail.com>
2018-11-06 16:40:41 +0000

    Inicio de recuperar datos del usuario.

--- app/src/main/java/miranda/david/da/practica_1dmg/main/MainRepository.java
+++ app/src/main/java/miranda/david/da/practica_1dmg/main/MainRepository.java
@@ -3,4 +3,6 @@ package miranda.david.da.practica_1dmg.main;
 interface MainRepository {
 
     void cerrarSesion();
+    void obtenerUsuario(String email);
+    void eliminarUsuario(String id);
 }
--- app/src/main/java/miranda/david/da/practica_1dmg/main/MainRepositoryImpl.java
+++ app/src/main/java/miranda/david/da/practica_1dmg/main/MainRepositoryImpl.java
@@ -1,6 +1,9 @@
 package miranda.david.da.practica_1dmg.main;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 import org.greenrobot.eventbus.EventBus;
 
@@ -9,13 +12,27 @@ import miranda.david.da.practica_1dmg.main.events.MainEvent;
 public class MainRepositoryImpl implements MainRepository {
 
 
+
     @Override
     public void cerrarSesion(){
         FirebaseAuth.getInstance().signOut();
         postEvent(MainEvent.ON_LOG_OUT);
     }
 
+    @Override
+    public void obtenerUsuario(String email){
+        final DatabaseReference usuarioRef = FirebaseDatabase.getInstance().getReference().child("usuarios");
+        Query query = usuarioRef.child("usuarios").orderByChild("email").equalTo(email);
 
+
+    }
+
+    @Override
+    public void eliminarUsuario(String id){
+        final DatabaseReference usuarioRef = FirebaseDatabase.getInstance().getReference().child("usuarios").child(id);
+        usuarioRef.removeValue();
+
+    }
 
     private void postEvent(int type) {
         MainEvent mainEvent = new MainEvent();
